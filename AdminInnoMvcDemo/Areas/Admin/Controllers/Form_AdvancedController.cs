56bf56b7574d21c374a893103e990e34a1226841
smakarenko <innovaciiv@gmail.com>
2015-11-22 02:58:00 +0300

    Add AdminTemplate to Mvc Project

---
+++ AdminInnoMvcDemo/Areas/Admin/Controllers/Form_AdvancedController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminInnoMvcDemo.Areas.Admin.Controllers
+{
+    public class Form_AdvancedController : Controller
+    {
+        // GET: Admin/Form_Advanced
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
