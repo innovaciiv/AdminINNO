a308a958137aa41c634371e9ad9bd29238f92bca
smakarenko <innovaciiv@gmail.com>
2015-11-22 16:15:53 +0300

    1. Add DataTadle 2. Add DataTable ListView Scafold (ListDataTable)

---
+++ AdminInnoMvcDemo/Areas/Admin/Controllers/Table_data_tablesController.cs
@@ -0,0 +1,26 @@
+﻿using AdminInnoMvcDemo.Areas.Admin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminInnoMvcDemo.Areas.Admin.Controllers
+{
+    public class Table_data_tablesController : Controller
+    {
+        // GET: Admin/Table_data_tables
+        public ActionResult Index()
+        {
+            var model = new DataTableViewModel();
+            return View(model.GetList());
+        }
+
+        // Scafolds generate List whith datatable (ListDataTable)
+        public ActionResult ListDataTable()
+        {
+            var model = new DataTableViewModel();
+            return View(model.GetList());
+        }
+    }
+}
---
+++ AdminInnoMvcDemo/Areas/Admin/Models/DataTableViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdminInnoMvcDemo.Areas.Admin.Models
+{
+    public class DataTableViewModel
+    {
+
+        public string RenderingEngine { get; set; }
+
+        public string Browser { get; set; }
+
+
+        public string Platform { get; set; }
+
+
+        public string EngineVersion { get; set; }
+
+
+        public string CSSGrade { get; set; }
+
+        public List<DataTableViewModel> GetList()
+        {
+            return new List<DataTableViewModel>()
+            {
+                new DataTableViewModel { RenderingEngine="Trident",  Browser = "InternetExplorer 4.0", Platform ="Win 95+",  EngineVersion = "4", CSSGrade = "X"},
+                new DataTableViewModel { RenderingEngine="Trident",  Browser = "InternetExplorer 4.0", Platform ="Win 95+",  EngineVersion = "4", CSSGrade = "X"},
+                new DataTableViewModel { RenderingEngine="Trident",  Browser = "InternetExplorer 4.0", Platform ="Win 95+",  EngineVersion = "4", CSSGrade = "X"},
+                new DataTableViewModel { RenderingEngine="Trident",  Browser = "InternetExplorer 4.0", Platform ="Win 95+",  EngineVersion = "4", CSSGrade = "X"},
+                new DataTableViewModel { RenderingEngine="Trident",  Browser = "InternetExplorer 4.0", Platform ="Win 95+",  EngineVersion = "4", CSSGrade = "X"},
+                new DataTableViewModel { RenderingEngine="Trident",  Browser = "InternetExplorer 4.0", Platform ="Win 95+",  EngineVersion = "4", CSSGrade = "X"},
+                new DataTableViewModel { RenderingEngine="Trident",  Browser = "InternetExplorer 4.0", Platform ="Win 95+",  EngineVersion = "4", CSSGrade = "X"},
+                new DataTableViewModel { RenderingEngine="Trident",  Browser = "InternetExplorer 4.0", Platform ="Win 95+",  EngineVersion = "4", CSSGrade = "X"},
+                new DataTableViewModel { RenderingEngine="Trident",  Browser = "InternetExplorer 4.0", Platform ="Win 95+",  EngineVersion = "4", CSSGrade = "X"},
+                new DataTableViewModel { RenderingEngine="Trident",  Browser = "InternetExplorer 4.0", Platform ="Win 95+",  EngineVersion = "4", CSSGrade = "X"},
+                new DataTableViewModel { RenderingEngine="Trident",  Browser = "InternetExplorer 4.0", Platform ="Win 95+",  EngineVersion = "4", CSSGrade = "X"},
+                new DataTableViewModel { RenderingEngine="Trident",  Browser = "InternetExplorer 4.0", Platform ="Win 95+",  EngineVersion = "4", CSSGrade = "X"},
+            };
+        }
+    }
+}
